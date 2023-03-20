0270eaba8c56b0458e7e677bba33a2c39feb8f6b
bavanraj-swivel <bavanraj@swivelgroup.com.au>
2023-03-20 12:41:49 +0530

    AwsHealthController added.

--- src/main/java/com/hilltop/reservation/controller/AwsHealthController.java
+++ src/main/java/com/hilltop/reservation/controller/AwsHealthController.java
@@ -3,6 +3,9 @@ package com.hilltop.reservation.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Aws health controller
+ */
 @RestController
 public class AwsHealthController {
 
